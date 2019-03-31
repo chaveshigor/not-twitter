1360056601a309cb715c4a68032f763f76c9235e
chaveshigor <hhigorsilvachaves@gmail.com>
2019-03-31 13:29:50 -0300

    safe socket2

--- src/backend/ws.js
+++ src/backend/ws.js
@@ -1,6 +1,6 @@
 import Ws from '@adonisjs/websocket-client';
 
-export const ws = Ws('wss://192.168.0.106:3333');
+export const ws = Ws(`wss://not-twitter-backend.herokuapp.com/`);
 
 export const chat = ws.subscribe('tweet')
 
